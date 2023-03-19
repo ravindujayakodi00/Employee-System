96df48b1dbe4d8a70b145e015d116fad4f1b8cd9
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-03-19 18:36:44 +0530

    Employee Controller and Model

---
+++ src/main/java/com/payroll/employeerest/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.payroll.employeerest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    
+
+}
---
+++ src/test/java/com/payroll/employeerest/EmployeerestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.employeerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
